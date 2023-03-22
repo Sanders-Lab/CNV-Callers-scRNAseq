121f7be1fae411616fc4952cb5a85bdcf124342f
Suharto Banerjee <suharto.banerjee@mdc-berlin.de>
2023-03-22 17:43:37 +0100

    modular numbat_dig_kar_plot
    made modular digKar plot to run with cmd_args.

--- R/numbat_dig_kar_plot.R
+++ R/numbat_dig_kar_plot.R
@@ -1,39 +1,55 @@
 library(tidyverse)
-
-
-joint_post <- read.table('../outputs/numbat_pbmc3k_epithelium_tnbc1/joint_post_2.tsv',
-                         sep = '\t',
-                         header = T)
-joint_post %>% head()
-
-
-segs_raw <- joint_post %>% select(cell,
-                                  CHROM,
-                                  cnv_state,
-                                  seg_start,
-                                  seg_end)
-
-segs_raw <- segs_raw %>% dplyr::rename(seqnames = 'CHROM',
-                                       start = 'seg_start',
-                                       end = 'seg_end')
-segs_raw$seqnames <- paste0('chr', segs_raw$seqnames)
-segs_raw %>% head()
-
-#### ---- PLOT DIGKARS ---- ####
-
-segs_raw %>% head()
-
 library(foreach)
 library(parallel)
 
-source('../../digital_karyotype/R/add_info_plt_layers.R')
-prj_id <- 'numbat_digital_karyotypes'
-tmp_plot <- '../plots/tmp/tmp_plot.pdf'
-cna_colors <- c('red', 'blue', 'green')
-names(cna_colors) <- c('amp', 'del', 'loh')
 
 
-plotDigKar <- function(cell_mat, cell_name){
+
+#### ---- BRIEF ---- ####
+
+# @param 1. joint_post_dir: (str) the dir of joint post tsv
+# @param 2. prj_id: (str) the dir name where the dig kars will 
+#           be saved.
+# @param 3. n_cores: (integer) the n_cores.
+
+
+
+
+
+
+ReadData <- function(joint_post_dir){
+
+    joint_post <- read.table(joint_post_dir,
+                             sep = '\t',
+                             header = T)
+
+    writeLines("Joint Post data read in!")
+
+    return(joint_post)
+
+}
+
+
+ProcData <- function(joint_post){
+
+    cna_segs <- joint_post %>% select(cell,
+                                      CHROM,
+                                      cnv_state,
+                                      seg_start,
+                                      seg_end)
+
+    cna_segs <- cna_segs %>% dplyr::rename(seqnames = 'CHROM',
+                                           start = 'seg_start',
+                                           end = 'seg_end')
+    cna_segs$seqnames <- paste0('chr', cna_segs$seqnames)
+
+    writeLines("Data Processed successfully!")
+    return(cna_segs)
+
+}
+
+
+PlotDigKar <- function(cell_mat, cell_name){
     
     cna_dk <- plotSkeletonH2(ideo_df)
 
@@ -51,21 +67,44 @@ plotDigKar <- function(cell_mat, cell_name){
 }
 
 
-n_cores <- detectCores() - 4
+
+
+#### ---- MAIN SECTION ---- #### 
+
+source('../../digital_karyotype/R/add_info_plt_layers.R')
+cmd_args <- commandArgs(trailingOnly = T)
+
+joint_post_dir <- cmd_args[1]
+prj_id <- cmd_args[2]
+n_cores <- as.integer(cmd_args[3])
+
+writeLines(str_glue("The input args are:\n
+           joint_post_dir: {joint_post_dir}\n
+           prj_id: {prj_id}\n
+           n_cores: {n_cores}\n"))
+
+joint_post <- ReadData(joint_post_dir)
+cna_segs <- ProcData(joint_post)
+
+
+cna_colors <- c('red', 'blue', 'green')
+names(cna_colors) <- c('amp', 'del', 'loh')
+
 cluster <- makeForkCluster(n_cores)
 doParallel::registerDoParallel(cluster)
 
 
-u_cells <- unique(segs_raw$cell)
+writeLines("Starting to plot Digital Karyotypes...")
+u_cells <- unique(cna_segs$cell)
 start <- Sys.time()
 foreach(c = 1:length(u_cells)) %dopar% {
-    
-    cell_name <- u_cells[c]
-    curr_cell_mat <- segs_raw %>% filter(cell == cell_name)
 
-    plotDigKar(curr_cell_mat, cell_name)
+    cell_name <- u_cells[c]
+    curr_cell_mat <- cna_segs %>% filter(cell == cell_name)
+
+    PlotDigKar(curr_cell_mat, cell_name)
 
 }
-end <- Sys.time()
-end - start
+Sys.time() - start
 parallel::stopCluster(cluster)
+
