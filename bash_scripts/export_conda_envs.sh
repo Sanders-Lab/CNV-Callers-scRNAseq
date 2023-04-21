92921163f8aa20282026dfb27a28d990744fac05
Suharto Banerjee <suharto.banerjee@mdc-berlin.de>
2023-04-21 15:36:31 +0200

    envs now save without date

--- bash_scripts/export_conda_envs.sh
+++ bash_scripts/export_conda_envs.sh
@@ -7,23 +7,22 @@
 # since both rely on jags and r-jags
 
 source ~/.bashrc
-DATE=$(date -I)
 
 
 
 conda activate numbat
 printf "\nExporting currently active env: %s" $(echo $CONDA_DEFAULT_ENV)
-conda env export > ../environments/${CONDA_DEFAULT_ENV}_${DATE}_env.yml
+conda env export > ../environments/${CONDA_DEFAULT_ENV}_env.yml
 printf "\n%s env exported successfully to the environments folder!" $(echo $CONDA_DEFAULT_ENV)
 
 
 conda activate infercnv
 printf "\nExporting currently active env: %s" $(echo $CONDA_DEFAULT_ENV)
-conda env export > ../environments/${CONDA_DEFAULT_ENV}_${DATE}_env.yml
+conda env export > ../environments/${CONDA_DEFAULT_ENV}_env.yml
 printf "\n%s env exported successfully to the environments folder!" $(echo $CONDA_DEFAULT_ENV)
 
 
 conda activate conicsmat
 printf "\nExporting currently active env: %s" $(echo $CONDA_DEFAULT_ENV)
-conda env export > ../environments/${CONDA_DEFAULT_ENV}_${DATE}_env.yml
+conda env export > ../environments/${CONDA_DEFAULT_ENV}_env.yml
 printf "\n%s env exported successfully to the environments folder!" $(echo $CONDA_DEFAULT_ENV)
