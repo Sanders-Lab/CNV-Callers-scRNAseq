a871848fe37fb2ffce66c09a2e3207a93dc8b741
Suharto Banerjee <suharto.banerjee@mdc-berlin.de>
2023-08-09 17:28:41 +0200

    bash scripts to run numbat and infercnv
    tested. Infercnv although stalls at gibbs step

---
+++ R_job_scripts/run_numbat_job.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+#SBATCH -o R_%10x_%j.out
+#SBATCH -e R_%10x_%j.out
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=64
+#SBATCH --mem-per-cpu=2G
+#SBATCH --time=10:00:00
+#SBATCH --mail-type=BEGIN,FAIL,END
+#SBATCH --mail-user=suharto.banerjee@mdc.berlin.de
+
+
+source ~/.bashrc
+conda activate numbat
+printf "\nConda env activated at: %s" $(echo $CONDA_DEFAULT_ENV)
+
+
+Rscript ../R/run_numbat.R \
+    "" \
+    "" \
+    ../data/ega_data/tall_ega_data/10X_count/bamtofastq/outs/filtered_feature_bc_matrix \
+    ../data/ega_data/tall_ega_data/pileup_phase_tall/tall_allele_counts.tsv.gz \
+    $SLURM_CPUS_PER_TASK \
+    ../outputs/tall_scnova/numbat_pbmc_tall/
